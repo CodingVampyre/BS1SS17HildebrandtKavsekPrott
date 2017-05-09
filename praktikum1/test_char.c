aac00e5ca18b54e56b324fc620bc5968f83cae82
NightmareEDM <lillykavsek@hotmail.com>
2017-05-09 11:32:06 +0200

    update

---
+++ praktikum1/test_char.c
@@ -0,0 +1,44 @@
+#include <stddef.h>
+#include <ctype.h>
+#include <stdio.h>
+
+getwords (char *line, char *words[], int maxwords) {
+  char *p = line;
+  int nwords = 0;
+
+  while (1) {
+    while (isspace(*p)) {
+      p++;
+    }
+
+    if (*p == "\0") {
+      return nwords;
+    }
+
+    words[nwords++] = p;
+
+    while (!isspace (*p) && *p != '\0') {
+      p++;
+    }
+    if (*p == '\0') {
+      return nwords;
+    }
+
+    *p++ = '\0';
+
+    if (nwords >= maxwords) {
+      return nwords;
+    }
+  }
+}
+
+int main () {
+  char line[] = "Mein Name ist Marvin!";
+  int i;
+  char *words[sizeof(line)/3];
+
+  int nwords = getwords(line, words, 10);
+  for (i = 0; i < nwords; i++) {
+    printf("%s\n", words[i]);
+  }
+}
