8dc900363f3f6a47124f7fcd5eb8eda15084ce35
tprott <tim.prott@freenet.de>
2017-05-22 17:11:12 +0200

    update server.c

--- prak1/server.c
+++ prak1/server.c
@@ -10,6 +10,11 @@
 
 int doprocessing (int sock);
 
+getwords(char *line, char *words[], int maxwords);
+int put(char* key, char* value, char* res);
+int get(char* key, char* res);
+int del(char* key, char* res);
+
 int main(int argc, char *argv[] ) {
 
   int sockfd, newsockfd, portno, clilen;
@@ -27,7 +32,7 @@ int main(int argc, char *argv[] ) {
 
   /* initialisiere socket */
   bzero((char *) &serv_addr, sizeof(serv_addr));
-  portno = 1945;
+  portno = 6969;
 
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = INADDR_ANY;
@@ -65,7 +70,7 @@ int main(int argc, char *argv[] ) {
         //exit(0);
 
         while (doprocessing(newsockfd) == 0) {
-          printf("Weiter!");
+
         }
         close(sockfd);
         exit(0);
@@ -82,32 +87,88 @@ int doprocessing (int sock) {
   char buffer[256];
   bzero(buffer, 256);
   n = read(sock,buffer,255);
+  char *words[sizeof(buffer)/2];
   char res[256];
+
+  int nwords = getwords(buffer, words, 10);
+
+
 
   if (n < 0) {
     perror("ERROR reading from socket");
     exit(1);
   }
 
-  strcpy(res, buffer);
-
-  printf(buffer);
-
-  printf("Here is the message: %s\n", buffer);
-  //n = write(sock, "I got your message\n",18);
-  char szOutput[256];
-  sprintf(szOutput, "Output: %s\n", res);
-  n = write(sock, szOutput, strlen(szOutput));
+  printf("Incomming: %s\n", buffer);
 
   if (n < 0) {
     perror("ERROR writing to socket");
     exit(1);
+  }
+  for (int h = 0; h < nwords; h++) {
+    printf("%s\n", words[h]);
   }
 
   if(strncmp(buffer, "EXIT", 4) == 0) {
     return 1;
   }
 
+  if (strncmp(words[0], "TST", 3) == 0){
+    printf("ERROR SUCCESS \n");
+  } else if(strncmp(words[0], "PUT", 3) == 0) {
+    put(words[1], words[2], res);
+  } else if(strncmp(words[0], "GET", 3) == 0) {
+    get(words[1], res);
+  } else if(strncmp(words[0], "DEL", 3) == 0) {
+    del(words[1], res);
+  }
+
+  char szOutput[256];
+  sprintf(szOutput, "Output: %s\n", res);
+  n = write(sock, szOutput, strlen(szOutput));
+
   return 0;
+
+}
+
+getwords (char *line, char *words[], int maxwords) {
+  char *p = line;
+  int nwords = 0;
+
+  while (1) {
+    while (isspace(*p)) {
+      p++;
+    }
+
+    if (*p == "\0") {
+      return nwords;
+    }
+
+    words[nwords++] = p;
+
+    while (!isspace (*p) && *p != '\0') {
+      p++;
+    }
+    if (*p == '\0') {
+      return nwords;
+    }
+
+    *p++ = '\0';
+
+    if (nwords >= maxwords) {
+      return nwords;
+    }
+  }
+}
+
+int put(char* key, char* value, char* res) {
+
+}
+
+int get(char* key, char* res) {
+
+}
+
+int del(char* key, char* res) {
 
 }
