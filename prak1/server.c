aaf88fef8f52d447894b6dd710e9910c61bf61cf
dev <lillykavsek@hotmail.de>
2017-05-23 17:28:29 +0200

    update server.c

--- prak1/server.c
+++ prak1/server.c
@@ -40,6 +40,8 @@ int main(int argc, char *argv[] ) {
 
   mem_id = shmget(IPC_PRIVATE, sizeof(struct KeyPair) * NUM_KEY_PAIRS, IPC_CREAT|0777);
   keys = (struct KeyPair*)shmat(mem_id, 0, 0);
+
+  printf("Keys, %i", keys);
   memset(keys, 0, sizeof(struct KeyPair) * NUM_KEY_PAIRS); // TODO Solve Segmentation fault 11 occuring here!
 
   /* socket funktion aufrufen */
