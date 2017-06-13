520ca782626c5cafee845d80f3d03fce536048ad
L. Kavsek <lillykavsek@hotmail.com>
2017-06-13 11:41:43 +0200

    Create server.c
    update layout -.-

--- prak1/server.c
+++ prak1/server.c
@@ -85,177 +85,179 @@ int main(int argc, char *argv[] ) {
 
   //INFINITE LOOP
   while (1) {
-    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
 
-    if (newsockfd < 0) {
-      perror("ERROR on accept");
-      exit(1);
-    }
+	if (newsockfd < 0) {
+		perror("ERROR on accept");
+		exit(1);
+	}
 
-    //create child process if needed
-    pid = fork();
+	//create child process if needed
+	pid = fork();
 
-    // DEBUG: result = shmctl(id, cmd, buffer);
+	if (pid < 0) {
+		perror("ERROR on fork");
+		exit(1);
+	}
 
-    if (pid < 0) {
-      perror("ERROR on fork");
-      exit(1);
-    }
-
-    //check if a new process was generated
-    if (pid == 0) {
+	//check if a new process was generated
+	if (pid == 0) {
 		
-	  // sem-ops
-	  struct sembuf enter, leave;
-	  enter.sem_num = leave.sem_num = 0; //0tes sem
-	  enter.sem_flg = leave.sem_flg = SEM_UNDO;
-	  enter.sem_op = -1; // block
-	  leave.sem_op = 1; // unblock
+		// sem-ops
+		struct sembuf enter, leave;
+		enter.sem_num = leave.sem_num = 0; //0tes sem
+		enter.sem_flg = leave.sem_flg = SEM_UNDO;
+		enter.sem_op = -1; // block
+		leave.sem_op = 1; // unblock
 		
-      //the process is kept alive until the client closes the connection
-      while (doprocessing(newsockfd) == 0) {}
-      //closes the socket and basic clean up of the shared memory
-      close(sockfd);
-      shmdt(keys);
-      shmctl(id, IPC_RMID, 0);
-      exit(0);
-    } else {
-        close(newsockfd);
-    }
-  }
+		//the process is kept alive until the client closes the connection
+		while (doprocessing(newsockfd) == 0) {}
+	    
+		//closes the socket and basic clean up of the shared memory
+		close(sockfd);
+		shmdt(keys);
+		shmctl(id, IPC_RMID, 0);
+		exit(0);
+	    
+		} else {
+			close(newsockfd);
+		}
+	}
 }
 
 //handle user input
 int doprocessing (int sock) {
-  int n;
-  char buffer[256];
-  bzero(buffer, 256);
-  n = read(sock,buffer,255);
-  char *words[sizeof(buffer)/2];
-  char res[256];
+	int n;
+	char buffer[256];
+	bzero(buffer, 256);
+	n = read(sock,buffer,255);
+	char *words[sizeof(buffer)/2];
+	char res[256];
 
-  //divide buffer into array of words
-  int nwords = getwords(buffer, words, 10);
+	//divide buffer into array of words
+	int nwords = getwords(buffer, words, 10);
 
-  //errorhandling
-  if (n < 0) {
-    perror("ERROR reading from socket");
-    exit(1);
-  }
+	//errorhandling
+	if (n < 0) {
+		perror("ERROR reading from socket");
+		exit(1);
+	}
 
-  printf("Incomming: %s\n", buffer);
+	printf("Incomming: %s\n", buffer);
 
-  if (n < 0) {
-    perror("ERROR writing to socket");
-    exit(1);
-  }
+	if (n < 0) {
+		perror("ERROR writing to socket");
+		exit(1);
+	}
 
-  //cancels the request by "EXIT"
-  if(strncmp(buffer, "EXIT", 4) == 0) {
-    return 1;
-  }
+	//cancels the request by "EXIT"
+	if(strncmp(buffer, "EXIT", 4) == 0) {
+		return 1;
+	}
 
-  //handle user input
-  if (strncmp(words[0], "TST", 3) == 0){
-    printf("ERROR SUCCESS \n");
-  } else if(strncmp(words[0], "PUT", 3) == 0) {
-    put(words[1], words[2], res);
-  } else if(strncmp(words[0], "GET", 3) == 0) {
-    get(words[1], res);
-  } else if(strncmp(words[0], "DEL", 3) == 0) {
-    del(words[1], res);
-  }
+	//handle user input
+	if (strncmp(words[0], "TST", 3) == 0){
+		printf("ERROR SUCCESS \n");
+	} else if(strncmp(words[0], "PUT", 3) == 0) {
+		put(words[1], words[2], res);
+	} else if(strncmp(words[0], "GET", 3) == 0) {
+		get(words[1], res);
+	} else if(strncmp(words[0], "DEL", 3) == 0) {
+		del(words[1], res);
+	}
 
-  //Output handling
-  char szOutput[256];
-  sprintf(szOutput, "Output: %s\n Id: %i\n", res, id);
-  n = write(sock, szOutput, strlen(szOutput)); //write answer to client
+	//Output handling
+	char szOutput[256];
+	sprintf(szOutput, "Output: %s\n Id: %i\n", res, id);
+	n = write(sock, szOutput, strlen(szOutput)); //write answer to client
 
-  return 0;
-
+	return 0;
 }
+
 //takes the string and returns an array of  words
 getwords (char *line, char *words[], int maxwords) {
-  char *p = line;
-  int nwords = 0;
+	char *p = line;
+	int nwords = 0;
 
-  while (1) {
-    while (isspace(*p)) {
-      p++;
-    }
+	while (1) {
+		while (isspace(*p)) {
+			p++;
+		}
 
-    if (*p == "\0") {
-      return nwords;
-    }
+		if (*p == "\0") {
+			return nwords;
+		}
 
-    words[nwords++] = p;
+		words[nwords++] = p;
 
-    while (!isspace (*p) && *p != '\0') {
-      p++;
-    }
-    if (*p == '\0') {
-      return nwords;
-    }
+		while (!isspace (*p) && *p != '\0') {
+			p++;
+		}
+		
+		if (*p == '\0') {
+			return nwords;
+		}
 
-    *p++ = '\0';
+		*p++ = '\0';
 
-    if (nwords >= maxwords) {
-      return nwords;
-    }
-  }
+		if (nwords >= maxwords) {
+			return nwords;
+		}
+	}
 }
 
 //checks if key is existing, if not then store in value into struct
 int put(char* key, char* value, char* res) {
   
-  // BEGIN: CRITICAL
+	// BEGIN: CRITICAL
   
-  for(int i = 0; i < NUM_KEY_PAIRS; i++) {
-    if (strlen(keys[i].key) == 0) {
-      strncpy(keys[i].key, key, sizeof(keys[i].key));
-      strncpy(keys[i].value, value, sizeof(keys[i].value));
-      return 0;
-    }
-  }
+	for(int i = 0; i < NUM_KEY_PAIRS; i++) {
+		if (strlen(keys[i].key) == 0) {
+			strncpy(keys[i].key, key, sizeof(keys[i].key));
+			strncpy(keys[i].value, value, sizeof(keys[i].value));
+			return 0;
+		}
+	}
   
-  // END: CRITICAL
+	// END: CRITICAL
   
-  strcpy(res, "NIL");
-  return 1;
+	strcpy(res, "NIL");
+	return 1;
 }
 
 //checks if key exists, if yes returns the value of the requested key
 int get(char* key, char* res) {
   
-  // BEGIN: CRITICAL
+	// BEGIN: CRITICAL
   
-  for(int i = 0; i < NUM_KEY_PAIRS; i++) {
-    if (strncmp(keys[i].key, key, sizeof(keys[i].key)) == 0) {
-      strcpy(res, keys[i].value);
-      return 0;
-    }
-  }
+	for(int i = 0; i < NUM_KEY_PAIRS; i++) {
+		if (strncmp(keys[i].key, key, sizeof(keys[i].key)) == 0) {
+			strcpy(res, keys[i].value);
+			return 0;
+		}
+	}
   
-  // END: CRITICAL
+	// END: CRITICAL
   
-  strcpy(res, "NIL");
-  return 1;
+	strcpy(res, "NIL");
+	return 1;
 }
+
 //checks if key exists, if yes deletes the values
 int del(char* key, char* res) {
 
-  // BEGIN: CRITICAL
+	// BEGIN: CRITICAL
   
-  for(int i = 0; i < NUM_KEY_PAIRS; i++) {
-    if (strncmp(keys[i].key, key, sizeof(keys[i].key)) == 0) {
-      strncpy(res, keys[i].value, sizeof(keys[i].value));
-      memset(&keys[i], 0, sizeof(keys[i]));
-      return 0;
-    }
-  }
+	for(int i = 0; i < NUM_KEY_PAIRS; i++) {
+		if (strncmp(keys[i].key, key, sizeof(keys[i].key)) == 0) {
+			strncpy(res, keys[i].value, sizeof(keys[i].value));
+			memset(&keys[i], 0, sizeof(keys[i]));
+			return 0;
+		}
+	}
   
-  // END: CRITICAL
+	// END: CRITICAL
   
-  strcpy(res, "NIL");
-  return 1;
+	strcpy(res, "NIL");
+	return 1;
 }
